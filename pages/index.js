5eac2651214c84367d0b1a66cbf8200f85d58fc6
Sanay Shah <sanayshah2@gmail.com>
2025-11-11 18:30:32 -0800

    Update paragraph text in Home component

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div>
       <h1>Static Next.js App</h1>
-      <p>This is a static site without SSR. I created this website using omega and git push</p>
+      <p>This is a static site without SSR. I created this website using service and git push</p>
     </div>
   )
 }
